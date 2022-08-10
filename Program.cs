4527d567f608aa4f4175d01cccbc1f97579e8205
Emiliya <107909207+marchenkoev@users.noreply.github.com>
2022-08-10 13:46:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+string[] array = new string[] {"2", ":-)", "hello", "world"};
+string[] n = new string[array.Length];
+void SecondArrayWithIF(string[] array, string[] n)
+{
+    for (int i = 0; i < array.Length; i=i+1)
+    {
+    if(array[i].Length <= 3)
+        {
+        n[count] = array[i];
+        count=count+1;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i=i+1)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array, n);
+PrintArray(n);
